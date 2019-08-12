3f0fa49a6f202dc3576c50ea5e70041b1b9c47cb
hainkiwanki <kevinr.be@gmail.com>
2019-08-12 23:18:19 +0200

    ADDED: Interactable

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,5 +2,52 @@
 
 public class Interactable : MonoBehaviour
 {
+    public float Radius => m_radius;
 
+    [SerializeField] float m_radius = 1.0f;
+
+    private bool m_isFocussed = false;
+    private bool m_hasInteracted = false;
+    private Transform m_player;
+
+    private void Update()
+    {
+        if(m_isFocussed && !m_hasInteracted)
+        {
+            float dist = Vector3.Distance(m_player.position, transform.position);
+            if(dist <= m_radius)
+            {
+                m_hasInteracted = true;
+                Interact();
+            }
+        }
+    }
+
+    protected virtual void Interact()
+    {
+        Debug.Log("Interacted");
+    }
+
+    public void OnFocus(Transform _playerTransform)
+    {
+        m_isFocussed = true;
+        m_player = _playerTransform;
+        m_hasInteracted = false;
+    }
+
+    public void OnForget()
+    {
+        m_isFocussed = false;
+        m_player = null;
+        m_hasInteracted = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (m_radius > 0.0f)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, m_radius);
+        }
+    }
 }
