e12d6eb073b60df6550f554a4970aafad52f5618
hainkiwanki <kevinr.be@gmail.com>
2019-08-13 22:54:30 +0200

    Messing around with screen resolution

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,24 +12,27 @@ public enum ECameraMode
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform m_player;
-    [SerializeField] ECameraMode m_mode;
 
     private Camera m_cam;
     private Transform m_destination = null;
     private Vector3 m_playerPos;
     private float m_offset = 0.0f;
 
-
-    [SerializeField] bool m_update = false;
-
     private void Awake()
     {
         m_cam = Camera.main;
         m_playerPos = m_player.position + Vector3.up * 0.5f;
 
+        var screens = Screen.resolutions;
+        var currentScreen = Screen.currentResolution;
+        for (int i = 0; i < screens.Length; i++)
+        {
+            if (screens[i].height == currentScreen.height && screens[i].width == currentScreen.width)
+                Debug.Log("This screen resolution is: " + screens[i]);
+        }
+
         // transform.rotation = Quaternion.Euler(70.0f, 45.0f, 0.0f);
         m_cam.orthographic = true;
-        m_cam.orthographicSize = 7.5f;
         transform.rotation = Quaternion.Euler(30.0f, 40.0f, 0.0f);
         m_offset = -50.0f;
         transform.position = m_playerPos + transform.forward * m_offset;
@@ -38,18 +41,19 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        if (m_update)
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        screenRatio /= (16.0f / 9.0f);
+        m_cam.orthographicSize = 7.5f * screenRatio;
+
+        if (m_destination != null)
         {
-            if (m_destination != null)
-            {
-                // Do something
-                Debug.Log("Must be doing something");
-            }
-            else
-            {
-                var destination = GetCameraPos(m_player.position);
-                transform.position = Vector3.Lerp(transform.position, destination, 1f);
-            }
+            // Do something
+            Debug.Log("Must be doing something");
+        }
+        else
+        {
+            var destination = GetCameraPos(m_player.position);
+            transform.position = Vector3.Lerp(transform.position, destination, 1f);
         }
     }
 
