ff226dd13ff05d2ffc21fb0bfb7c54451bc2c9e5
hainkiwanki <kevinr.be@gmail.com>
2020-02-10 21:22:31 +0100

    UI Changes + Animations package

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private TabGroup m_tabGroup;
+
+    [HideInInspector]
+    public Image m_background;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        m_tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_tabGroup.OnTabExit(this);
+    }
+
+    private void Start()
+    {
+        m_background = GetComponent<Image>();
+        m_tabGroup.Subscribe(this);
+    }
+}
